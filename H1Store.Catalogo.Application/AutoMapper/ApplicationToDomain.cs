816f0e998d1dcede0467eb8a7cb7c7a5434349d7
Gabriela Reis <100102288+GabbiReis@users.noreply.github.com>
2023-10-22 15:41:35 -0300

    Add files via upload

---
+++ H1Store.Catalogo.Application/AutoMapper/ApplicationToDomain.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using H1Store.Catalogo.Application.ViewModels;
+using H1Store.Catalogo.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1Store.Catalogo.Application.AutoMapper
+{
+	public class ApplicationToDomain : Profile
+	{
+		public ApplicationToDomain()
+		{
+            CreateMap<CartaViewModel, Carta>()
+                .ConstructUsing(c => new Carta(c.Nome, c.Endereco, c.Idade, c.Conteudo));
+
+            CreateMap<NovaCartaViewModel, Carta>()
+               .ConstructUsing(c => new Carta(c.Nome, c.Endereco, c.Idade, c.Conteudo));
+
+
+
+        }
+	}
+}
